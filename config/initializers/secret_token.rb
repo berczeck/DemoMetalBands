ba2388954f7b85f02e954bc1371e89de58e194e7
Luis Alexander Aldazabal Gil <berczeck_400@hotmail.com>
2012-03-15 00:30:47 -0500

    carga inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoMetalBands::Application.config.secret_token = 'c88ae8e2e3e8bf9927f0eb5b9f54761a761fed94e4f0bf97ecabac2c460c600a7a66745a7f68aa7590851eeb3dbba19cab400a7974ca52ddbd297f0c0d8c00e8'
